ba88020e1ce16055e8696de8c40aa046adb382bf
Richard M. Davis <vendor_github@ddg.mx>
2024-07-04 21:51:38 -0600

    fixes2 - 20240704

--- update_config.sh
+++ update_config.sh
@@ -10,6 +10,31 @@ validate() {
     fi
 }
 
+
+
+
+# Check if docker-compose is installed
+if ! command -v docker-compose &> /dev/null
+then
+
+figlet "Installing docker-compose"
+    echo "docker-compose not found. Installing..."
+    sudo apt-get update
+    sudo apt-get -y install docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    docker-compose --version
+    validate "Installing docker-compose"
+else
+    echo "docker-compose is already installed."
+    docker-compose --version
+fi
+
+
+
+
+
 figlet "Starting Config"
 
 # Copy the example config file
