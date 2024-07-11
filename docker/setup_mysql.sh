2fb5c86158efbbf9ecdffe1fef0c21a39659768f
Richard M. Davis <vendor_github@ddg.mx>
2024-07-11 11:13:47 -0600

    fixes5

--- docker/setup_mysql.sh
+++ docker/setup_mysql.sh
@@ -6,3 +6,7 @@ server_id=3${numeric_part}
 
 # Create the replication configuration
 echo -e "[mysqld]\nserver_id=${server_id}\nlog_bin=mysql-bin\ngtid_mode=ON\nenforce-gtid-consistency=ON\nbinlog-format=ROW\nport=3316\nbind-address=0.0.0.0" > replication.cnf
+
+
+# Print the generated file for verification
+cat replication.cnf
