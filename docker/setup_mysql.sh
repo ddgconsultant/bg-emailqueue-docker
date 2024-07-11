3b6f8626042846141421163718414d512903d366
Richard M. Davis <vendor_github@ddg.mx>
2024-07-11 11:24:59 -0600

    fixes6

--- docker/setup_mysql.sh
+++ docker/setup_mysql.sh
@@ -3,6 +3,7 @@
 # Extract numeric part from hostname
 numeric_part=$(hostname | grep -o -E '[0-9]+')
 server_id=3${numeric_part}
+rm -rf replication.cnf
 
 # Create the replication configuration
 echo -e "[mysqld]\nserver_id=${server_id}\nlog_bin=mysql-bin\ngtid_mode=ON\nenforce-gtid-consistency=ON\nbinlog-format=ROW\nport=3316\nbind-address=0.0.0.0" > replication.cnf
